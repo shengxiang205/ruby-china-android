315e31478c907dccc74afea43c75ac9be542b11b
盛翔 <shengxiang205@foxmail.com>
2016-09-25 20:16:12 +0800

    enable webveiw debug mode and turbo link debug log

--- app/src/main/java/org/ruby_china/android/Application.java
+++ app/src/main/java/org/ruby_china/android/Application.java
@@ -1,5 +1,8 @@
 package org.ruby_china.android;
 
+import android.webkit.WebView;
+
+import com.basecamp.turbolinks.TurbolinksSession;
 import com.facebook.drawee.backends.pipeline.Fresco;
 
 public class Application extends android.app.Application {
@@ -7,5 +10,20 @@ public class Application extends android.app.Application {
     public void onCreate() {
         super.onCreate();
         Fresco.initialize(this);
+        if (BuildConfig.DEBUG) {
+            this.enableDebugMode();
+        }
+    }
+
+    /**
+     * enable turbolink debug log
+     * enable webview debug mode
+     *
+     * @see {@link WebView#setWebContentsDebuggingEnabled(boolean)}
+     * @see {@link TurbolinksSession#setDebugLoggingEnabled(boolean)}
+     */
+    private void enableDebugMode() {
+        TurbolinksSession.getDefault(this).setDebugLoggingEnabled(true);
+        WebView.setWebContentsDebuggingEnabled(true);
     }
 }
